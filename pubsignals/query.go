0b8001bf1ef0288e32eb912704951c1b4cd36632
ilya-korotya <officalbatya@gmail.com>
2023-03-15 18:55:50 +0200

    support xsd types

--- pubsignals/query.go
+++ pubsignals/query.go
@@ -13,6 +13,7 @@ import (
 	jsonSuite "github.com/iden3/go-schema-processor/json"
 	"github.com/iden3/go-schema-processor/merklize"
 	"github.com/iden3/go-schema-processor/utils"
+	"github.com/piprate/json-gold/ld"
 	"github.com/pkg/errors"
 )
 
@@ -72,17 +73,21 @@ func (q Query) Check(
 		return err
 	}
 
-	if err := q.verifyCredentialSubject(pubSig, verifiablePresentation); err != nil {
+	schemaBytes, _, err := loader.Load(ctx, q.Context)
+	if err != nil {
+		return fmt.Errorf("failed load schema by context: %w", err)
+	}
+
+	if err := q.verifyCredentialSubject(
+		pubSig,
+		verifiablePresentation,
+		schemaBytes,
+	); err != nil {
 		return err
 	}
 
 	if !q.SkipClaimRevocationCheck && pubSig.IsRevocationChecked == 0 {
 		return errors.New("check revocation is required")
-	}
-
-	schemaBytes, _, err := loader.Load(ctx, q.Context)
-	if err != nil {
-		return fmt.Errorf("failed load schema by context: %w", err)
 	}
 
 	return q.verifyClaim(ctx, schemaBytes, pubSig)
@@ -155,21 +160,46 @@ func (q Query) verifySchemaID(pubSig *CircuitOutputs) error {
 	return ErrSchemaID
 }
 
-func (q Query) verifyCredentialSubject(pubSig *CircuitOutputs, verifiablePresentation json.RawMessage) error {
+func (q Query) verifyCredentialSubject(
+	pubSig *CircuitOutputs,
+	verifiablePresentation json.RawMessage,
+	ctxBytes []byte,
+) error {
 	fieldName, predicate, err := extractQueryFields(q.CredentialSubject)
 	if err != nil {
 		return err
 	}
 
+	var fieldType string
+	if fieldName != "" {
+		fieldType, err = merklize.TypeFromContext(
+			ctxBytes,
+			fmt.Sprintf("%s.%s", q.Type, fieldName),
+		)
+		if err != nil {
+			return err
+		}
+	}
+
 	if q.CredentialSubject != nil && len(predicate) == 0 {
 		ctx := context.Background()
-		if err := q.validateDisclosure(ctx, pubSig, fieldName, verifiablePresentation); err != nil {
+		if err := q.validateDisclosure(
+			ctx,
+			pubSig,
+			fieldName,
+			fieldType,
+			verifiablePresentation,
+		); err != nil {
 			return err
 		}
 		return nil
 	}
 
-	values, operator, err := parseFieldPredicate(predicate)
+	if fieldType == ld.XSDString {
+		return errors.New("xsd:string type is supported only for disclosure request")
+	}
+
+	values, operator, err := parseFieldPredicate(fieldType, predicate)
 	if err != nil {
 		return err
 	}
@@ -203,7 +233,13 @@ func (q Query) verifyCredentialSubject(pubSig *CircuitOutputs, verifiablePresent
 	return nil
 }
 
-func (q Query) validateDisclosure(ctx context.Context, pubSig *CircuitOutputs, key string, verifiablePresentation json.RawMessage) error {
+func (q Query) validateDisclosure(
+	ctx context.Context,
+	pubSig *CircuitOutputs,
+	key string,
+	fieldType string,
+	verifiablePresentation json.RawMessage,
+) error {
 	if verifiablePresentation == nil {
 		return errors.New("selective disclosure value is missed")
 	}
@@ -223,36 +259,19 @@ func (q Query) validateDisclosure(ctx context.Context, pubSig *CircuitOutputs, k
 		return errors.Errorf("failed to merklize doc: %v", err)
 	}
 
-	merkalizedPath, err := merklize.NewPathFromDocument(verifiablePresentation, fmt.Sprintf("verifiableCredential.%s", key))
+	merklizedPath, err := merklize.NewPathFromDocument(verifiablePresentation, fmt.Sprintf("verifiableCredential.%s", key))
 	if err != nil {
 		return errors.Errorf("failed build path to '%s' key: %v", key, err)
 	}
 
-	valueByPath, err := mz.RawValue(merkalizedPath)
+	valueByPath, err := mz.RawValue(merklizedPath)
 	if err != nil {
 		return errors.Errorf("failed get raw value: %v", err)
 	}
 
-	// all int values in JSON are float64 since mz.MkValue
-	// function supports only int representation convert float64
-	// to int with check accuracy of int
-	vt, ok := valueByPath.(float64)
-	if ok {
-		intVal := int64(vt)
-		if float64(intVal) != vt {
-			return errors.New("invalid int value")
-		}
-		valueByPath = intVal
-	}
-
-	mvValue, err := mz.MkValue(valueByPath)
+	mvBig, err := merklize.HashValue(fieldType, valueByPath)
 	if err != nil {
-		return errors.Errorf("failed mz value: %v", err)
-	}
-
-	mvBig, err := mvValue.MtEntry()
-	if err != nil {
-		return errors.Errorf("failed big.Int value mz from : %v", err)
+		return errors.Errorf("failed to hash value: %v", err)
 	}
 
 	if pubSig.Value[0].Cmp(mvBig) != 0 {
@@ -262,22 +281,22 @@ func (q Query) validateDisclosure(ctx context.Context, pubSig *CircuitOutputs, k
 	return nil
 }
 
-func parseFieldPredicate(fieldPredicate map[string]interface{}) ([]*big.Int, int, error) {
-	var (
-		values   []*big.Int
-		operator int
-		err      error
-	)
-
+func parseFieldPredicate(
+	fieldType string,
+	fieldPredicate map[string]interface{},
+) (
+	values []*big.Int,
+	operator int,
+	err error,
+) {
 	for op, v := range fieldPredicate {
-
 		var ok bool
 		operator, ok = circuits.QueryOperators[op]
 		if !ok {
 			return nil, 0, errors.New("query operator is not supported")
 		}
 
-		values, err = getValuesAsArray(v)
+		values, err = getValuesAsArray(v, fieldType)
 		if err != nil {
 			return nil, 0, err
 		}
@@ -309,21 +328,27 @@ func extractQueryFields(req map[string]interface{}) (fieldName string, fieldPred
 	return fieldName, fieldPredicate, nil
 }
 
-func getValuesAsArray(v interface{}) ([]*big.Int, error) {
+func getValuesAsArray(v interface{}, valueType string) ([]*big.Int, error) {
 	var values []*big.Int
 
-	switch value := v.(type) {
-	case float64:
-		values = make([]*big.Int, 1)
-		values[0] = new(big.Int).SetInt64(int64(value))
-	case []interface{}:
-		values = make([]*big.Int, len(value))
-		for i, item := range value {
-			values[i] = new(big.Int).SetInt64(int64(item.(float64)))
+	listOfValues, ok := v.([]interface{})
+	if ok {
+		values = make([]*big.Int, len(listOfValues))
+		for i, item := range listOfValues {
+			hashedValue, err := merklize.HashValue(valueType, item)
+			if err != nil {
+				return nil, err
+			}
+			values[i] = hashedValue
 		}
-	default:
-		return nil, errors.Errorf("unsupported values type %T", v)
+		return values, nil
 	}
+
+	hashedValue, err := merklize.HashValue(valueType, v)
+	if err != nil {
+		return nil, err
+	}
+	values = append(values, hashedValue)
 
 	return values, nil
 }
--- pubsignals/query_test.go
+++ pubsignals/query_test.go
@@ -17,8 +17,13 @@ var (
 	iid, _      = new(big.Int).SetString("22638457188543025296541325416907897762715008870723718557276875842936181250", 10)
 	issuerID, _ = core.IDFromInt(iid)
 
-	schemaHashInt, _ = big.NewInt(0).SetString("336615423900919464193075592850483704600", 10)
-	coreSchema       = core.NewSchemaHashFromInt(schemaHashInt)
+	schemaHashKYCEmployee, _ = big.NewInt(0).SetString("40507426258035268209384482717564233104", 10)
+	KYCEmployeeSchema        = core.NewSchemaHashFromInt(schemaHashKYCEmployee)
+
+	schemaHashKYCCountry, _ = big.NewInt(0).SetString("336615423900919464193075592850483704600", 10)
+	KYCCountrySchema        = core.NewSchemaHashFromInt(schemaHashKYCCountry)
+
+	bigIntTrueHash, _ = big.NewInt(0).SetString("18586133768512220936620570745912940619677854269274689475585506675881198879027", 10)
 )
 
 type mockMemorySchemaLoader struct {
@@ -80,6 +85,37 @@ func (r *mockMemorySchemaLoader) Load(_ context.Context, _ string) (schema []byt
             "@type": "xsd:integer"
           }
         }
+      },
+	  "KYCEmployee": {
+        "@id": "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld#KYCEmployee",
+        "@context": {
+          "@version": 1.1,
+          "@protected": true,
+          "id": "@id",
+          "type": "@type",
+          "kyc-vocab": "https://github.com/iden3/claim-schema-vocab/blob/main/credentials/kyc.md#",
+          "xsd": "http://www.w3.org/2001/XMLSchema#",
+          "documentType": {
+            "@id": "kyc-vocab:documentType",
+            "@type": "xsd:integer"
+          },
+          "ZKPexperiance": {
+            "@id": "kyc-vocab:hasZKPexperiance",
+            "@type": "xsd:boolean"
+          },
+          "hireDate": {
+            "@id": "kyc-vocab:hireDate",
+            "@type": "xsd:dateTime"
+          },
+          "position": {
+            "@id": "kyc-vocab:position",
+            "@type": "xsd:string"
+          },
+          "salary": {
+            "@id": "kyc-vocab:salary",
+            "@type": "xsd:double"
+          }
+        }
       }
     }
   ]
@@ -96,6 +132,19 @@ var vp = []byte(`{
 	"verifiableCredential": {
 		"@type": "KYCCountryOfResidenceCredential",
 		"countryCode": 800
+	}
+}`)
+
+// TODO(illia-korotia): update link in context after providing new schema.
+var vpEmployee = []byte(`{
+	"@context": [
+		"https://www.w3.org/2018/credentials/v1",
+		"https://gist.githubusercontent.com/ilya-korotya/55630d9b93fc526f96bba3734153939e/raw/cebe9e357861a64635a79fee4fa79fb67b27a58b/test"
+	],
+	"@type": "VerifiablePresentation",
+	"verifiableCredential": {
+		"@type": "KYCEmployee",
+		"position": "SSI Consultant"
 	}
 }`)
 
@@ -120,7 +169,7 @@ func TestCheckRequest_Success(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -143,7 +192,7 @@ func TestCheckRequest_Success(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -154,6 +203,58 @@ func TestCheckRequest_Success(t *testing.T) {
 				IsRevocationChecked: 1,
 			},
 			vp: vp,
+		},
+		{
+			name: "Query with boolean type",
+			query: Query{
+				AllowedIssuers: []string{"*"},
+				CredentialSubject: map[string]interface{}{
+					"ZKPexperiance": map[string]interface{}{
+						"$eq": true,
+					},
+				},
+				Context: "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+				Type:    "KYCEmployee",
+			},
+			pubSig: &CircuitOutputs{
+				IssuerID:    &issuerID,
+				ClaimSchema: KYCEmployeeSchema,
+				ClaimPathKey: func() *big.Int {
+					v, _ := big.NewInt(0).SetString("1944808975288007371356450257872165609440470546066507760733183342797918372827", 10)
+					return v
+				}(),
+				Operator:            1,
+				Value:               []*big.Int{bigIntTrueHash},
+				Merklized:           1,
+				IsRevocationChecked: 1,
+			},
+		},
+		{
+			name: "Selective disclosure with xsd:string type",
+			query: Query{
+				AllowedIssuers: []string{"*"},
+				CredentialSubject: map[string]interface{}{
+					"position": map[string]interface{}{},
+				},
+				Context: "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+				Type:    "KYCEmployee",
+			},
+			pubSig: &CircuitOutputs{
+				IssuerID:    &issuerID,
+				ClaimSchema: KYCEmployeeSchema,
+				ClaimPathKey: func() *big.Int {
+					v, _ := big.NewInt(0).SetString("15406634529806189041952040954758558497189093183268091368437514469450172572054", 10)
+					return v
+				}(),
+				Operator: 1,
+				Value: func() []*big.Int {
+					v, _ := big.NewInt(0).SetString("957410455271905675920624030785024750144198809104092676617070098470852489834", 10)
+					return []*big.Int{v}
+				}(),
+				Merklized:           1,
+				IsRevocationChecked: 1,
+			},
+			vp: vpEmployee,
 		},
 	}
 
@@ -186,7 +287,7 @@ func TestCheckRequest_SelectiveDisclosure_Error(t *testing.T) {
 			vp: nil,
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -211,7 +312,7 @@ func TestCheckRequest_SelectiveDisclosure_Error(t *testing.T) {
 			vp: vp,
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -236,7 +337,7 @@ func TestCheckRequest_SelectiveDisclosure_Error(t *testing.T) {
 			vp: vp,
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -261,7 +362,7 @@ func TestCheckRequest_SelectiveDisclosure_Error(t *testing.T) {
 			vp: vp,
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -286,7 +387,7 @@ func TestCheckRequest_SelectiveDisclosure_Error(t *testing.T) {
 			vp: vp,
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				ClaimPathKey: func() *big.Int {
 					v, _ := big.NewInt(0).SetString("17002437119434618783545694633038537380726339994244684348913844923422470806844", 10)
 					return v
@@ -334,7 +435,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 			},
 			expErr: ErrSchemaID,
 		},
@@ -351,7 +452,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 			},
 			expErr: errors.New("multiple requests not supported"),
 		},
@@ -367,7 +468,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 			},
 			expErr: errors.New("failed cast type map[string]interface"),
 		},
@@ -386,12 +487,12 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 			},
 			expErr: errors.New("multiple predicates for one field not supported"),
 		},
 		{
-			name: "Proof was generated for another query",
+			name: "Proof was generated for another query operator",
 			query: Query{
 				AllowedIssuers: []string{issuerDID},
 				Context:        "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
@@ -404,7 +505,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				Operator:    3,
 			},
 			expErr: ErrRequestOperator,
@@ -416,14 +517,14 @@ func TestCheckRequest_Error(t *testing.T) {
 				Context:        "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
 				Type:           "KYCCountryOfResidenceCredential",
 				CredentialSubject: map[string]interface{}{
-					"countyCode": map[string]interface{}{
+					"countryCode": map[string]interface{}{
 						"$nin": []interface{}{float64(20)},
 					},
 				},
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:    &issuerID,
-				ClaimSchema: coreSchema,
+				ClaimSchema: KYCCountrySchema,
 				Operator:    5,
 				Value:       []*big.Int{big.NewInt(40)},
 			},
@@ -443,7 +544,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:            &issuerID,
-				ClaimSchema:         coreSchema,
+				ClaimSchema:         KYCCountrySchema,
 				ClaimPathKey:        big.NewInt(0),
 				Operator:            5,
 				Value:               []*big.Int{big.NewInt(20)},
@@ -466,7 +567,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:            &issuerID,
-				ClaimSchema:         coreSchema,
+				ClaimSchema:         KYCCountrySchema,
 				Operator:            5,
 				Value:               []*big.Int{big.NewInt(20)},
 				Merklized:           0,
@@ -490,7 +591,7 @@ func TestCheckRequest_Error(t *testing.T) {
 			},
 			pubSig: &CircuitOutputs{
 				IssuerID:            &issuerID,
-				ClaimSchema:         coreSchema,
+				ClaimSchema:         KYCCountrySchema,
 				Operator:            5,
 				Value:               []*big.Int{big.NewInt(20)},
 				Merklized:           0,
@@ -498,6 +599,35 @@ func TestCheckRequest_Error(t *testing.T) {
 				IsRevocationChecked: 0,
 			},
 			expErr: errors.New("check revocation is required"),
+		},
+		{
+			name: "Non disclosuer request with xsd:string type",
+			query: Query{
+				AllowedIssuers: []string{issuerDID},
+				Context:        "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+				Type:           "KYCEmployee",
+				CredentialSubject: map[string]interface{}{
+					"position": map[string]interface{}{
+						"$eq": "SSI Consultant",
+					},
+				},
+			},
+			pubSig: &CircuitOutputs{
+				IssuerID:    &issuerID,
+				ClaimSchema: KYCEmployeeSchema,
+				ClaimPathKey: func() *big.Int {
+					v, _ := big.NewInt(0).SetString("15406634529806189041952040954758558497189093183268091368437514469450172572054", 10)
+					return v
+				}(),
+				Operator: 1,
+				Value: func() []*big.Int {
+					v, _ := big.NewInt(0).SetString("957410455271905675920624030785024750144198809104092676617070098470852489834", 10)
+					return []*big.Int{v}
+				}(),
+				Merklized:           1,
+				IsRevocationChecked: 1,
+			},
+			expErr: errors.New("xsd:string type supported only for disclosure request"),
 		},
 	}
 
