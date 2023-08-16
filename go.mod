a4998d851ddd04906f1bcec965c1dbbc782785ea
Ilya <officalbatya@gmail.com>
2023-08-16 19:08:49 +0300

    send auth and proof notifications (#57)
    * send auth and proof notifications
    * add constructor for create contract invoke message

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/iden3/go-rapidsnark/types v0.0.3
 	github.com/iden3/go-rapidsnark/verifier v0.0.5
 	github.com/iden3/go-schema-processor v1.3.1
-	github.com/iden3/iden3comm v1.1.0
+	github.com/iden3/iden3comm v1.1.1-0.20230810141028-42068fe8ad57
 	github.com/ipfs/go-ipfs-api v0.6.0
 	github.com/piprate/json-gold v0.5.1-0.20230111113000-6ddbe6e6f19f
 	github.com/pkg/errors v0.9.1
---
+++ transport/notification.go
@@ -0,0 +1,47 @@
+package transport
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/iden3/go-schema-processor/verifiable"
+	"github.com/iden3/iden3comm/protocol"
+	"github.com/iden3/iden3comm/transport/notification"
+	"github.com/pkg/errors"
+)
+
+// SendPushAuthRequest sends authorization request to the user via a push notification.
+func SendPushAuthRequest(
+	ctx context.Context,
+	diddoc verifiable.DIDDocument,
+	authMsg protocol.AuthorizationRequestMessage,
+) (*notification.UserNotificationResult, error) {
+	authMsgBytes, err := json.Marshal(authMsg)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return notification.Notify(
+		ctx,
+		authMsgBytes,
+		diddoc,
+		nil,
+	)
+}
+
+// SendPushContractInvokeRequest sends a contract invoke request to the user via a push notification.
+func SendPushContractInvokeRequest(
+	ctx context.Context,
+	diddoc verifiable.DIDDocument,
+	contractInvokeMsg protocol.ContractInvokeRequestMessage,
+) (*notification.UserNotificationResult, error) {
+	ciMsgBytes, err := json.Marshal(contractInvokeMsg)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return notification.Notify(
+		ctx,
+		ciMsgBytes,
+		diddoc,
+		nil,
+	)
+}
